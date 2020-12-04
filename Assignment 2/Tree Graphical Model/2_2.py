09007edc4ef4cb4e6a2524797cf94ee7bc452b32
FilipBergentoft <2012.filip.bergentoft@gmail.com>
2020-12-04 13:53:30 +0100

    Worked on 2.2

--- Assignment 2/Tree Graphical Model/2_2.py
+++ Assignment 2/Tree Graphical Model/2_2.py
@@ -41,34 +41,64 @@ def calculate_likelihood(tree_topology, theta, beta):
     This is a suggested template. You don't have to use it.
     """
 
-    # TODO Add your code here
 
-    # Start: Example Code Segment. Delete this segment completely before you implement the algorithm.
-    print("Calculating the likelihood...")
-    likelihood = np.random.rand()
+    # tree_topology: index corresponds to node, element corresponds to parent of that node, nan := root node
+    # K defines the number of nodes in the tree
+    K = np.asarray(theta).shape[1]
+    number_of_nodes = np.asarray(theta).shape[0]
+    node_ids = range(number_of_nodes-1,-1,-1)
+
+    # Matrix of probabilities, each row coresponds to a node
+    S = np.empty((number_of_nodes,K))
+    
+    # Walking backwards in the nodes
+    for node_id in node_ids:
+
+        # If leaf, do this:
+        if node_id not in tree_topology:
+            
+            leaf_value = np.int(beta[node_id])
+            s = np.zeros((K))
+            # Setting the probability of the observed variable to 1
+            s[leaf_value] = 1
+            S[node_id,:] = s
+
+        # If not leaf, do this:
+        else:
+            # Using the structure of tree_topology to find the node's children            
+            children_node_id = np.where(tree_topology == node_id)[0]
+            s = np.ones(K)
+
+            for child_node_id in children_node_id:
+                theta_child = np.stack(theta[child_node_id],axis=0)
+                s_child = S[child_node_id,:]
+                prob_child = np.matmul(theta_child, s_child)
+                s = np.multiply(s, prob_child)
+            S[node_id,:] = s
+
+    s_root = S[0,:]
+    theta_root = np.stack(theta[0],axis=0)
+    likelihood = np.dot(s_root, theta_root)
     # End: Example Code Segment
 
     return likelihood
 
 
 def main():
-    print("Hello World!")
-    print("This file is the solution template for question 2.2.")
-
     print("\n1. Load tree data from file and print it\n")
-    directory = '/Users/filipbergentoft/Desktop/Github/DD2434/Assignment 2/'
-    filename = directory + "data/q2_2/q2_2_small_tree.pkl"  # "data/q2_2/q2_2_medium_tree.pkl", "data/q2_2/q2_2_large_tree.pkl"
+    directory = '/Users/filipbergentoft/Desktop/Github/DD2434/Assignment 2/Tree Graphical Model/'
+    #qqq: Need to change this to this years verison!!
+    filename = directory + "data/q2_2/q2_2_medium_tree.pkl"  # "data/q2_2/q2_2_medium_tree.pkl", "data/q2_2/q2_2_large_tree.pkl"
     t = Tree()
     t.load_tree(filename)
-    t.print()
-    t.print_topology()
+    #t.print()
+    #t.print_topology()
     print("K of the tree: ", t.k, "\talphabet: ", np.arange(t.k))
 
     print("\n2. Calculate likelihood of each FILTERED sample\n")
     # These filtered samples already available in the tree object.
     # Alternatively, if you want, you can load them from corresponding .txt or .npy files
-
-    for sample_idx in range(t.num_samples):
+    for sample_idx in range(5):
         beta = t.filtered_samples[sample_idx]
         print("\n\tSample: ", sample_idx, "\tBeta: ", beta)
         sample_likelihood = calculate_likelihood(t.get_topology_array(), t.get_theta_array(), beta)
